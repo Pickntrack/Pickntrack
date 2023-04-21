fb6d62ffc6045cba4e0e64ecb9a1aa11670bc74e
Vivek Goswami <vivekgoswami7383@gmail.com>
2023-04-21 20:23:16 +0530

    EJS Removed

--- src/index.js
+++ src/index.js
@@ -3,13 +3,9 @@ const app = express();
 require("dotenv").config();
 const { databaseConnection } = require("./database/connection");
 const cors = require("cors");
-const ejs = require("ejs");
 const path = require("path");
 
 const port = process.env.PORT;
-
-app.set("view engine", "ejs");
-app.set("views", "./views");
 
 app.use(express.json());
 app.use(cors());
